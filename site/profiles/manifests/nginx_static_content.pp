8ec43961f717a803f2f6d0ba08e11181261c356b
Radomir Mocarski <R.Mocarski@kainos.com>
2017-09-05 19:17:11 +0100

    added option to create multiple subfolder and a bit of readme

--- site/profiles/manifests/nginx_static_content.pp
+++ site/profiles/manifests/nginx_static_content.pp
@@ -1,8 +1,46 @@
+# Class profile::nginx_static_content
+#
+# Sets up Nginx service for hosting static content.
+#
+# == Dependencies
+# - maestrodev/wget
+# - jfryman/nginx
+#
+# === Parameters
+# [*listen_port*]
+# Port that Nginx will be listening on.
+# Default: '8085'
+#
+# [*server_name*]
+# Server name for the site.
+# Default: 'static_content'
+#
+# [*root_dir*]
+# Root directory for files subdirectories.
+# Default: '/srv/www'
+#
+# [*file_dirs*]
+# List of subdirectories for storing files.
+# Default: ['/srv/www/files']
+#
+# [*owner*]
+# User permissions for the files.
+# Default: 'root'
+#
+# [*group*]
+# Group permissions for the files.
+# Default: 'nginx'
+#
+# [*download_files*]
+# Hash of files to download. Uses maestrodev/wget module (https://github.com/maestrodev/puppet-wget).
+# Default: {}
+#
+
 class profiles::nginx_static_content (
   $listen_port    = '8085',
   $server_name    = 'static_content',
   $root_dir       = '/srv/www/',
-  $file_dir       = '/srv/www/files',
+  $file_dirs      = ['/srv/www/files'],
   $owner          = 'root',
   $group          = 'nginx',
   $download_files = {},
@@ -20,7 +58,7 @@ class profiles::nginx_static_content (
     require => Package['nginx'],
   }
 
-  file { $file_dir:
+  file { $file_dirs:
     ensure  => directory,
     owner   => $owner,
     group   => $group,
@@ -35,7 +73,7 @@ class profiles::nginx_static_content (
     www_root    => $root_dir,
     server_name => [$server_name],
     autoindex   => 'on',
-    require     => File[$file_dir]
+    require     => File[$file_dirs]
   }
 
   selinux::module { 'nginx_unreservedport':
@@ -50,7 +88,7 @@ class profiles::nginx_static_content (
 
   $download_files_defaults = {
     mode    => '0755',
-    require => File[$file_dir],
+    require => File[$file_dirs],
     notify  => Exec['update_files_ownership']
   }
 
