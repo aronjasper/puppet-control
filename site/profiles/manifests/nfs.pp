17fbef37c72cf5ec9808faa95958b1fe92a4b4b9
Chris Hatton <christopher.hatton@digital.landregistry.gov.uk>
2017-09-08 17:10:41 +0100

    new profile for nfs, corrections 3

--- site/profiles/manifests/nfs.pp
+++ site/profiles/manifests/nfs.pp
@@ -30,10 +30,10 @@ class profiles::nfs (
   # creates a directory for mount only if it does not exist
 
   exec { 'create_nfs_directory':
-    command  => "mkdir ${mount_point}",
-    unless   => "test -d ${mount_point}",
-    path     => '/usr/local/bin/:/bin/',
-    before   => Mount[$mount_point]
+    command => "mkdir ${mount_point}",
+    unless  => "test -d ${mount_point}",
+    path    => '/usr/local/bin/:/bin/',
+    before  => Mount[$mount_point]
   }
 
 
