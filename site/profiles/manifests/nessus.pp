f6da8161d5cbca927bef7fec62dc2e248d0a1d1f
lukaszre-kainos <lukasz.reszke@digital.landregistry.gov.uk>
2017-10-18 09:00:54 +0100

    modified manifest for nessus

--- site/profiles/manifests/nessus.pp
+++ site/profiles/manifests/nessus.pp
@@ -8,12 +8,12 @@
 class profiles::nessus(
   $package_source = undef,
   $package_name   = undef,
+  $package_ensure = undef,
 ) {
   package { 'nessus_rpm':
-    ensure   => $::package_ensure,
+    ensure   => $package_ensure,
     provider => 'rpm',
     source   => $package_source,
     name     => $package_name,
   }
-  include nessus
 }
