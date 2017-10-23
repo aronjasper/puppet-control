4461c03a784bc39133fc954f4620381887107816
Paul Wright <paul.wright@digital.landregistry.gov.uk>
2017-10-23 10:36:37 +0100

    Add ntp monitoring

--- site/profiles/manifests/generic_monitoring.pp
+++ site/profiles/manifests/generic_monitoring.pp
@@ -7,6 +7,8 @@
 #                                check_service_procs command
 #   monitor_port               - the port(s) to check for, passed to the nagios check_service_tcp
 #                                command. Supports a single value or an array.
+#   monitor_ntp                - if true, a check for a ntp will be created.
+#
 #   min_process_warning_count  - generate a warning alert if the number of processes
 #                                falls below this number
 #   max_process_warning_count  - generate a warning alert if the number of processes
@@ -22,6 +24,7 @@ class profiles::generic_monitoring(
 
   $monitor_service            = false,
   $monitor_port               = false,
+  $monitor_ntp                = false,
   $min_process_warning_count  = 1,
   $max_process_warning_count  = 10,
   $min_process_critical_count = 1,
@@ -68,5 +71,22 @@ class profiles::generic_monitoring(
 
   if $monitor_port {
     create_port_check { $monitor_port: }
+  }
+
+  if $monitor_ntp {
+    @@nagios_service { "${::hostname}-lr-${name}-ntp_check" :
+      ensure                => present,
+      check_command         => "check_nrpe!check_service_ntp\\!'127.0.0.1'",
+      mode                  => '0644',
+      owner                 => root,
+      use                   => 'generic-service',
+      host_name             => $::hostname,
+      check_period          => $time_period,
+      contact_groups        => 'admins',
+      notification_interval => 0,
+      notifications_enabled => 1,
+      notification_period   => $time_period,
+      service_description   => 'LR ntp check'
+    }
   }
 }
